ccb7f2545595a4e04270d4995bc4771aca94b0e2
jainul abushu <63139122+jainulabushu@users.noreply.github.com>
2021-03-18 20:40:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+index.js always remains same
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
